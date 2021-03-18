8a1a9cc63a536015ffc4935ec73a7ea131b935a4
Jose Ricardo Silveira <ricardusilveira@gmail.com>
2021-03-18 09:12:38 -0300

    Formatado pontuação e espaçamento para PEP8

--- sdss_spectra_v4.py
+++ sdss_spectra_v4.py
@@ -9,17 +9,18 @@ Created on Tue Jul  7 13:00:29 2020
 
 import pandas as pd
 from func import sdss_spec
-#test_size = 20, 25, 30
 
 savein = True
 
-                        
+# Bastava criar uma função em que baixasse uma lista de espectros com base na
+# lista de arquivos pelos quais obtivemos plate, fiberID, MJD
+
 tfp_tfn_splus_r = pd.read_csv('tfp_tfn_splus_r_v3.csv')
-#lista_pmf_splus_r = sdss_spec(tfp_tfn_splus_r, 'specs S-PLUS r', save = savein)
+lista_pmf_splus_r = sdss_spec(tfp_tfn_splus_r, 'specs S-PLUS r', save=savein)
 
 tfp_tfn_gaia = pd.read_csv('tfp_tfn_gaia_v3.csv')
-#lista_pmf_gaia = sdss_spec(tfp_tfn_gaia, 'specs GAIA', save = savein)
+lista_pmf_gaia = sdss_spec(tfp_tfn_gaia, 'specs GAIA', save=savein)
 
 tfp_tfn_gaia_splus = pd.read_csv('tfp_tfn_gaia_splus_r_v3.csv')
-#lista_pmf_gaia_splus = sdss_spec(tfp_tfn_gaia_splus, 'specs GAIA + S-PLUS r',\
-#                                  save = savein)
+lista_pmf_gaia_splus = sdss_spec(tfp_tfn_gaia_splus, 'specs GAIA + S-PLUS r',
+                                 save=savein)
